86436f4eaf98d2b0add0dcf747ee7fc0737f1441
Yashpawar-2005 <yashpawar12312@gmail.com>
2025-05-19 11:33:51 +0530

    i don't know what the hell was the issue with shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button'
+import { Button } from '@/components/ui/button'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className='bg-black'></div>
-     <Button></Button>
+     <div className='bg-black text-white'>hi</div>
+     <Button>jj</Button>
     </>
   )
 }
