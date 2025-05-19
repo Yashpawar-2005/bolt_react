2ec6d213a2b5f6a0ef8ce01011b71164d1202fca
Yashpawar-2005 <yashpawar12312@gmail.com>
2025-05-19 09:25:43 +0530

    changed the vite.config.ts to enalbe polling

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, 
+    watch: {
+      usePolling: true, 
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
